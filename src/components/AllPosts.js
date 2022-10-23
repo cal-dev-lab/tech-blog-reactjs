4d0e3763e0ed42962d0e79bfbfb9fc7f181eb929
Callum Lees <dev.cal.lees@gmail.com>
2022-10-23 23:47:17 +0100

    Added temp styling to blog post  cards

--- src/components/AllPosts.js
+++ src/components/AllPosts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
+import { Container } from "../containers/Container"
 
 export default function AllPosts() {
     const [allPostsData, setAllPosts] = useState(null);
@@ -24,22 +25,28 @@ export default function AllPosts() {
         }, []);
 
     return (
-        <div>
-            <h2>Blog Posts</h2>
-            <h3>Welcome to my blog posts page!</h3>
-            <div>
-                {allPostsData &&
+        <Container>
+            <h2 className="text-gray-100 font-publicb text-9xl mb-5">Blog Posts</h2>
+            <div className="grid grid-cols-4 mx-auto gap-x-5">
+            {allPostsData &&
                 allPostsData.map((post, index) => (
                     <Link to={"/" + post.slug.current + "/"} key={post.slug.current}>
-                        <span key={index}>
-                            <img src={post.mainImage.asset.url} alt={post.title} />
-                            <span>
-                                <h2>{post.title}</h2>
-                            </span>
-                        </span>
+                        <div key={index}>
+                            <img 
+                                className="h-48 w-full object-cover rounded-md"
+                                src={post.mainImage.asset.url} 
+                                alt={post.title} 
+                            />
+                            <div className="flex rounded-b-md bg-gray-100 p-4 h-20 my-auto items-center">
+                                <h2 className="text-xl font-publicb tracking-wide bg-gray-100">
+                                    {post.title}
+                                </h2>
+                            </div>
+                        </div>
                     </Link>
-                ))}
+                )
+            )}
             </div>
-        </div>
+        </Container>
     );
 }
