7760793e233cc61cb204ae0caeb005e298b6d694
Callum Lees <dev.cal.lees@gmail.com>
2022-10-23 23:30:44 +0100

    Added apiVersion

--- src/client.js
+++ src/client.js
@@ -4,4 +4,5 @@ export default sanityClient({
     projectId: "2lp2jieg", // find this at manage.sanity.io or in your sanity.json
     dataset: "production", // this is from those question during 'sanity init'
     useCdn: true,
+    apiVersion: '2021-10-21',
 });
